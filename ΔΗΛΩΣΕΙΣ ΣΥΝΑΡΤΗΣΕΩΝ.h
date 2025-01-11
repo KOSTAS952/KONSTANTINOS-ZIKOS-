28d1b9c192e24814f199883a73e89a6d0e2a228b
KOSTAS952 <kzikos777@gmail.com>
2025-01-12 00:06:07 +0200

    Update ΔΗΛΩΣΕΙΣ ΣΥΝΑΡΤΗΣΕΩΝ.h

--- ΔΗΛΩΣΕΙΣ ΣΥΝΑΡΤΗΣΕΩΝ.h
+++ ΔΗΛΩΣΕΙΣ ΣΥΝΑΡΤΗΣΕΩΝ.h
@@ -1,1 +1,14 @@
+// Επεξεργάζεται την παραγγελία ενός πελάτη
+void process_order(int product_id, int customer_pid);
 
+// Δημιουργεί τους αγωγούς και fork-άρει τις διεργασίες πελατών
+void create_pipes_and_fork_customers();
+
+// Αρχικοποιεί τον κατάλογο προϊόντων και τις στατιστικές
+void initialize_catalog();
+
+// Παράγει την τελική αναφορά παραγγελιών
+void generate_report();
+
+// Εμφανίζει το σύνολο των αγορών κάθε πελάτη
+void print_customer_totals();
